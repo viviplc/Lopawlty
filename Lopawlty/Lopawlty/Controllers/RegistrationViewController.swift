562cdfefa2daa0143748054bff33facc84c69577
Romeno <drf325@nyu.edu>
2021-11-17 21:04:22 -0500

    Implemented partial register function in register view controller to save data

--- Lopawlty/Lopawlty/Controllers/RegistrationViewController.swift
+++ Lopawlty/Lopawlty/Controllers/RegistrationViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class RegistrationViewController: UIViewController , UITextFieldDelegate {
 
@@ -54,15 +55,44 @@ class RegistrationViewController: UIViewController , UITextFieldDelegate {
         }
         
         let newUser = User(fullName: fullname, email: email, password: password, petType: petType);
+         
+         register()
         
         print(newUser.firebaseDictionary)
         */
+        register(name: fullname, email: email, password: password, petType: String(petTypeIndex), province: "Toronto", address: "Waterloo 1234");
                 
     }
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+    
+    func register(name: String, email : String, password: String, petType: String, province: String, address: String) {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+            return
+          }
+          
+          // 1
+          let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let customer = Customer(context: managedContext)
+        customer.name = name;
+        customer.email = email;
+        customer.password = password;
+        customer.petType = petType;
+        customer.province = province;
+        customer.address = address;
+        
+        do{
+                       try managedContext.save()
+            print("SUCCESS!");
+                   } catch{
+                       print("Error during saving data : \(error)")
+                   }
+        
     }
     
 
