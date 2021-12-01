623fb6b3530b8964fd867bd6dd178aee925121cf
Viviana <vivi.plc95@hotmail.com>
2021-11-30 19:40:17 -0500

    Revert "fix product detail view"
    This reverts commit 23fa40e0ea1f04ef8584072db31e092a2d79ce10.

--- Lopawlty/Lopawlty/Controllers/ProductsListViewController.swift
+++ Lopawlty/Lopawlty/Controllers/ProductsListViewController.swift
@@ -8,11 +8,9 @@
 import UIKit
 import Firebase
 
-class ProductsListViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate {
+class ProductsListViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate{
    
     @IBOutlet weak var productsListtable: UITableView!
-    @IBOutlet weak var BtnDogsFilter: UIButton!
-    @IBOutlet weak var BtnCatsFilter: UIButton!
     
     let searchController = 	UISearchController(searchResultsController: nil)
     
@@ -27,7 +25,8 @@ class ProductsListViewController: UIViewController, UISearchBarDelegate, UITable
         
         productsListtable.delegate = self
         productsListtable.dataSource = self
- 
+        
+        //SampleData.createSampleData()
         let db = Firestore.firestore()
         db.collection("Products")
             .getDocuments() { (querySnapshot, err) in
@@ -43,14 +42,6 @@ class ProductsListViewController: UIViewController, UISearchBarDelegate, UITable
                     
                 }
         }
-
-        BtnDogsFilter.layer.borderColor = UIColor(red: 0, green: 122/255, blue: 255/255, alpha: 1).cgColor
-        BtnDogsFilter.layer.cornerRadius = 10
-        BtnDogsFilter.layer.borderWidth = 1
-        BtnCatsFilter.layer.borderColor = UIColor(red: 0, green: 122/255, blue: 255/255, alpha: 1).cgColor
-        BtnCatsFilter.layer.cornerRadius = 10
-        BtnCatsFilter.layer.borderWidth = 1
-
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
@@ -78,34 +69,7 @@ class ProductsListViewController: UIViewController, UISearchBarDelegate, UITable
         cell.productPrice.text = prdPriceLbl
         cell.productImage.image = prdImageLbl
         
-        cell.BtnSeeMore.tag = indexPath.row
-        cell.BtnSeeMore.addTarget(self, action: #selector(ProductsListViewController.buttonTapped(_:)), for: UIControl.Event.touchUpInside)
-        
         return cell
-    }
-    
-    @objc func buttonTapped(_ sender:UIButton!){
-        self.performSegue(withIdentifier: "productListToSeeMore", sender: sender)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if (segue.identifier == "productListToSeeMore") {
-            if let seeMoreDestination = segue.destination as? SeeMoreViewController {
-                if let seeMoreBtn:UIButton = sender as! UIButton? {
-                    let prdImage:String! = self.products[seeMoreBtn.tag].imageFileName
-                    let prdname:String! = self.products[seeMoreBtn.tag].name
-                    let prdBrand:String! = self.products[seeMoreBtn.tag].brand
-                    let prdPrice:String! = "$\(self.products[seeMoreBtn.tag].price)"
-                    if (prdname != nil && prdImage != nil && prdBrand != nil && prdPrice != nil ){
-                        seeMoreDestination.readProductImage = prdImage!
-                        seeMoreDestination.readProductName = prdname!
-                        seeMoreDestination.readProductBrand = prdBrand!
-                        seeMoreDestination.readProductPrice = prdPrice!
-                    }
-                    
-                }
-            }
-        }
     }
 
 }
