7e99b6c6f5c03f538914909d718b030d3faeb19f
Viviana <vivi.plc95@hotmail.com>
2021-12-03 17:17:35 -0500

    tab bar images and profile view

---
+++ Lopawlty/Lopawlty/Controllers/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  Lopawlty
+//
+//  Created by user193926 on 12/3/21.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var BtnLogOut: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        BtnLogOut.layer.cornerRadius = 15
+    }
+    
+
+    
+
+}
